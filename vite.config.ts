fa95ceee6e70cbd7f72c45d93c579a5a48568960
Navdeep Singh <nsingh@gtalocal.com>
2025-08-19 19:02:04 -0400

    add server configuration to allow specific hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: ['gtalocal.com']
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
